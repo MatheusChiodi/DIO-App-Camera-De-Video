1f7d81ba5d98de474109551a47d166c805061022
Matheus Chiodi <matheuschiodi20@gmail.com>
2024-01-10 07:13:38 -0300

    fazendo imports e requisitando permissão

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState, useRef } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Camera } from 'expo-camera';
+import { Video } from 'expo-av';
+import { shareAsync } from 'expo-sharing';
+import * as MediaLibrary from 'expo-media-library';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const microphonePermission =
+        await Camera.requestMicrophonePermissionsAsync();
+      const mediaLibraryPermission =
+        await MediaLibrary.requestPermissionsAsync();
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
